87fdc1907c817eb40c417ba4b2dab33b9e630838
saurabh <saurabh.cthombre@gmail.com>
2024-02-10 17:01:34 +0530

    Update PracticeHelloThere.java

--- code/PracticeHelloThere.java
+++ code/PracticeHelloThere.java
@@ -3,6 +3,6 @@ package code;
 public class PracticeHelloThere {
 
     public static void main(String[] args) {
-        System.out.println("Hello there again!");
+        System.out.println("Hello there again from Remote!");
     }
 }
